f402491b8ac188f49777ea6b08c4f7dcf5e1812f
joyanta debnath <joydeb050@gmail.com>
2023-09-15 16:55:56 -0500

    minor

--- driver.py
+++ driver.py
@@ -10,74 +10,32 @@ from pem import *
 from base64 import *
 from verifySignature import *
 
-# def decodePem(filename):
-#     try:
-#         cert_list = []
-#         for c in parse_file(filename):
-#             if (isinstance(c, Certificate)):
-#                 cert_list.append(b64decode(str(c)[28:-26]))
-#             else:
-#                 return None
-#         return cert_list
-#     except:
-#         return None
-
 def main():
 
     ### command-line argument processing
-    # usage: ./armor-driver [-h] [--chain INPUT] [--trust_store CA_STORE] [--purpose CHECK_PURPOSE [CHECK_PURPOSE ...]]
+    # usage: ./armor-driver [-h] [--chain INPUT] [--trust_store CA_STORE] [--purpose CHECK_PURPOSE] 
     parser = argparse.ArgumentParser(description='ARMOR command-line arguments')
     parser.add_argument('--chain', type=str,
                         help='Input certificate chain location')
     parser.add_argument('--trust_store', type=str, default='/etc/ssl/certs/ca-certificates.crt',
                         help='Trust anchor location; default=/etc/ssl/certs/ca-certificates.crt')
-    parser.add_argument('--purpose', nargs='+',
-                        help='list of expected purposes of end-user certificate: serverAuth, clientAuth, codeSigning, emailProtection, timeStamping, OCSPSigning, digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment, keyAgreement, keyCertSign, cRLSign, encipherOnly, decipherOnly; default=anyPurpose',
-                        default=[])
+    parser.add_argument('--purpose', type=str,
+                        help='expected purpose for end-user certificate: serverAuth, clientAuth, codeSigning, emailProtection, timeStamping, or OCSPSigning, default=anyPurpose')
     args = parser.parse_args()
 
     input_chain = args.chain
     input_CA_store = args.trust_store
-    input_purposes = args.purpose
+    input_purpose = args.purpose
 
-    for purpose in input_purposes:
-        if purpose == 'serverAuth':
-            continue
-        elif purpose == 'clientAuth':
-            continue
-        elif purpose == 'codeSigning':
-            continue
-        elif purpose == 'emailProtection':
-            continue
-        elif purpose == 'timeStamping':
-            continue
-        elif purpose == 'OCSPSigning':
-            continue
-        elif purpose == 'digitalSignature':
-            continue
-        elif purpose == 'nonRepudiation':
-            continue
-        elif purpose == 'keyEncipherment':
-            continue
-        elif purpose == 'dataEncipherment':
-            continue
-        elif purpose == 'keyAgreement':
-            continue
-        elif purpose == 'keyCertSign':
-            continue
-        elif purpose == 'cRLSign':
-            continue
-        elif purpose == 'encipherOnly':
-            continue
-        elif purpose == 'decipherOnly':
-            continue
-        else:
+    if (input_purpose != 'serverAuth' and \
+        input_purpose != 'clientAuth' and \
+        input_purpose != 'codeSigning' and \
+        input_purpose != 'emailProtection' and \
+        input_purpose != 'timeStamping' and \
+        input_purpose != 'OCSPSigning' and input_purpose != None):
             print(
-                "Error : Purposes are not supported (supported purposes: serverAuth, "
-                "clientAuth, codeSigning, emailProtection, timeStamping, OCSPSigning, "
-                "digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment, "
-                "keyAgreement, keyCertSign, cRLSign, encipherOnly, decipherOnly"
-                ")")
+            "Error : Purposes are not supported (supported purposes: serverAuth, "
+            "clientAuth, codeSigning, emailProtection, timeStamping, OCSPSigning")
             sys.exit(-1)
 
     if not (input_chain.endswith((".pem", ".crt")) \
@@ -112,30 +70,10 @@ def main():
     readData(filename_aeres_output)
     os.remove(filename_aeres_output)
 
-    purpose_verify_res = verifyCertificatePurpose(input_purposes)
+    purpose_verify_res = verifyCertificatePurpose(input_purpose)
     if not purpose_verify_res:
         print("Error: Incorrect certificate purpose")
         return False
-
-    # decoded_rootcert_bytes = decodePem(input_CA_store)
-    # decoded_certchain_bytes = decodePem(input_chain)
-    # trusted_ca_index = -1
-    # if (decoded_rootcert_bytes == None):
-    #     print("Error: Failed to decode input PEM trusted CA certs")
-    #     return False
-    # elif (decoded_certchain_bytes == None):
-    #     print("Error: Failed to decode input PEM certificate chain")
-    #     return False
-    # else:
-    #     for i in range(0, len(decoded_certchain_bytes)):
-    #         if decoded_certchain_bytes[i] in decoded_rootcert_bytes:
-    #             print("Trusted CA: passed")
-    #             trusted_ca_index = i
-    #             break
-
-    # if trusted_ca_index == -1:
-    #     print("Trusted CA: failed")
-    #     return False
 
     sign_verify_res = verifySignatures()
     if not sign_verify_res:
