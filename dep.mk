71c1112cd24a27af34afad390b7011ce06f510e1
Zhengyao Lin <zhengyao-lin@outlook.com>
2024-12-05 21:44:26 -0500

    Build system tweak

--- dep.mk
+++ dep.mk
@@ -17,64 +17,74 @@ rwildcard=$(foreach d,$(wildcard $(1:=/*)),$(call rwildcard,$d,$2) $(filter $(su
 
 SOURCE = $(call rwildcard,src,*.rs)
 
+# Do no delete intermediates
+.SECONDARY:
+
 .PHONY: debug
-debug: verify
+debug: target/debug/$(NAME).verusdata
 	cargo build
 
 .PHONY: release
-release: verify
+release: target/release/$(NAME).verusdata
 	cargo build --release
-
-.PHONY: verify
-verify: target/verify/$(NAME).verusdata
 
 .PHONY: test
 test: $(TEST_TARGETS)
 	cargo test
 
-.PHONY: verify-deps
-verify-deps:
+.PHONY: verify-deps-%
+verify-deps-%:
 # Generate meta data for Rust dependencies
 	@set -e; \
 	for dep in $(CARGO_DEPS); do \
-		cargo build --profile=verify --package=$$dep; \
+		cmd="cargo build $(if $(filter release,$*),--release,) --package=$$dep"; \
+		echo "$$cmd"; \
+		$$cmd; \
     done
 
 # For each $dep in VERUS_DEPS, generate a rule to compile target/$dep.verusdata
 define DEP_TEMPLATE
-../$1/target/verify/$1.verusdata: $$(call rwildcard,../$1/src,*.rs)
+../$1/target/%/lib$1.rlib: $$(call rwildcard,../$1/src,*.rs)
 	@echo "### Verifying Verus dependency $1 (../$1)"
-	cd ../$1 && make target/verify/$1.verusdata
+	cd ../$1 && make target/$$*/lib$1.rlib
 endef
 $(foreach dep,$(VERUS_DEPS),$(eval $(call DEP_TEMPLATE,$(dep))))
+
+define VERUS_COMMAND
+verus $(if $(filter lib,$(TYPE)),$(LIB_MAIN),$(EXEC_MAIN)) \
+	--crate-name $(NAME) \
+	$(if $(filter lib,$(TYPE)),--crate-type=lib,) \
+	-L dependency=target/$1/deps \
+	$(foreach dep,$(VERUS_DEPS),-L dependency=../$(dep)/target/$1/deps) \
+	$(foreach dep,$(VERUS_DEPS),-L dependency=../$(dep)/target/$1) \
+	$(foreach dep,$(CARGO_DEPS),--extern $(subst -,_,$(dep))=$(firstword \
+		$(wildcard target/$1/lib$(subst -,_,$(dep)).rlib) \
+		$(wildcard target/$1/lib$(subst -,_,$(dep)).so) \
+		$(wildcard target/$1/lib$(subst -,_,$(dep)).dylib) \
+		$(wildcard target/$1/lib$(subst -,_,$(dep)).rmeta))) \
+	$(foreach dep,$(VERUS_DEPS), \
+		--extern $(dep)=$(firstword $(wildcard ../$(dep)/target/$1/lib$(dep).rlib)) \
+		--import $(dep)=../$(dep)/target/$1/$(dep).verusdata) \
+	$(VERUS_FLAGS)
+endef
 
 # NOTE: target/$(NAME).verusdata and target/$(NAME).rlib generated
 # by this rule is only supposed to be used for verification purposes
 # Use `cargo build` to build the crate for execution.
 #
-# Each dependency <dep> in CARGO_DEPS is mapped to verus argument --extern <dep>=target/verify/lib<dep>.<rlib|dylib|...>
+# Each dependency <dep> in CARGO_DEPS is mapped to verus argument --extern <dep>=target/<profile>/lib<dep>.<rlib|dylib|...>
 # Each dependency <dep> in VERUS_DEPS is mapped to verus argument
-#     --extern <dep>=../<dep>/target/verify/lib<dep>.rlib
-#     --import <dep>=../<dep>/target/verify/<dep>.verusdata
-target/verify/$(NAME).verusdata: $(SOURCE) verify-deps $(foreach dep,$(VERUS_DEPS),../$(dep)/target/verify/$(dep).verusdata)
-	mkdir -p target/verify
-	verus $(if $(filter lib,$(TYPE)),$(LIB_MAIN),$(EXEC_MAIN)) \
-		--crate-name $(NAME) \
-		$(if $(filter lib,$(TYPE)),--crate-type=lib,) \
-		-L dependency=target/verify/deps \
-		$(foreach dep,$(VERUS_DEPS),-L dependency=../$(dep)/target/verify/deps) \
-		$(foreach dep,$(VERUS_DEPS),-L dependency=../$(dep)/target/verify) \
-		$(foreach dep,$(CARGO_DEPS),--extern $(subst -,_,$(dep))=$(firstword \
-			$(wildcard target/verify/lib$(subst -,_,$(dep)).rlib) \
-			$(wildcard target/verify/lib$(subst -,_,$(dep)).so) \
-			$(wildcard target/verify/lib$(subst -,_,$(dep)).dylib) \
-			$(wildcard target/verify/lib$(subst -,_,$(dep)).rmeta))) \
-		$(foreach dep,$(VERUS_DEPS), \
-			--extern $(dep)=$(firstword $(wildcard ../$(dep)/target/verify/lib$(dep).rlib)) \
-			--import $(dep)=../$(dep)/target/verify/$(dep).verusdata) \
-		--export target/verify/$(NAME).verusdata \
-		--compile -o target/verify/lib$(NAME).rlib \
-		$(VERUS_FLAGS)
+#     --extern <dep>=../<dep>/target/<profile>/lib<dep>.rlib
+#     --import <dep>=../<dep>/target/<profile>/<dep>.verusdata
+target/%/$(NAME).verusdata: $(SOURCE) verify-deps-% $(foreach dep,$(VERUS_DEPS),../$(dep)/target/%/lib$(dep).rlib)
+	mkdir -p target/$*
+	$(call VERUS_COMMAND,$*) --export target/$*/$(NAME).verusdata
+
+target/%/lib$(NAME).rlib: $(SOURCE) verify-deps-% $(foreach dep,$(VERUS_DEPS),../$(dep)/target/%/lib$(dep).rlib)
+	mkdir -p target/$*
+	$(call VERUS_COMMAND,$*) \
+		--export target/$*/$(NAME).verusdata \
+		--compile -o target/$*/lib$(NAME).rlib
 
 .PHONY: clean
 clean:
