4945775278610a58b72d1cbb3bdbac64d84c472d
Zhengyao Lin <zhengyao-lin@outlook.com>
2025-01-01 16:41:05 -0500

    Add Makefile

--- build-ceres.sh
+++ build-ceres.sh
@@ -3,15 +3,15 @@
 cur=$(pwd)
 ./cleanup.sh
 
-ca_store=${1:-"/etc/ssl/certs/ca-certificates.crt"}
-echo "CA certificate store: $ca_store"
+# ca_store=${1:-"/etc/ssl/certs/ca-certificates.crt"}
+# echo "CA certificate store: $ca_store"
 
 mkdir -p build
 
 ## install pre-requisite packages
 sudo apt-get update
 sudo apt-get -y install python3 python3-pip ghc libghc-regex-compat-dev libghc-text-icu-dev
-pip3 install parsec pyinstaller pysmt --break-system-packages
+pip3 install parsec pysmt --break-system-packages
 
 cd src/extras/stringprep
 ./build.sh
